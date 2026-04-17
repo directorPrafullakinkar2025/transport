8bfa8104c128533b6448d74647047de85750f99e
prafulla <directorprafullakinkar2025@gmail.com>
2026-04-17 12:38:26 +0530

    dd

--- booking_entry.php
+++ booking_entry.php
@@ -61,6 +61,7 @@ $cityList = mysqli_query($conn, "SELECT * FROM city_master ORDER BY city_name AS
 <html>
 <head>
     <title>Booking Entry</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="/assets/css/layout.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
