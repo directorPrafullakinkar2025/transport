0c7da9e60dcc852cf917560ec8b0d5b16bf001ce
prafulla <directorprafullakinkar2025@gmail.com>
2026-04-17 12:02:06 +0530

    db

--- db.php
+++ db.php
@@ -1,12 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-// Debug: Check if the ID is actually being received
-echo "Checking LR ID: " . htmlspecialchars($_GET['lr_id'] ?? 'MISSING');
-?>
-<?php
 // $conn = new mysqli("localhost","root", "", "updated_transport_project");
 $conn = new mysqli("localhost", "shreeinfotechsof", ",oSjcFMm,Rg;", "shreeinfotechsof_updated_transport_project");
 if ($conn->connect_error) {
