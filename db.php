9e4a90dd5da0c2923c92ab455ced39293f70fb45
prafulla <directorprafullakinkar2025@gmail.com>
2026-04-17 11:25:01 +0530

    db

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-$conn = new mysqli("localhost","root", "", "updated_transport_project");
-// $conn = new mysqli("localhost", "shreeinfotechsof", ",oSjcFMm,Rg;", "updated_transport_project");
+// $conn = new mysqli("localhost","root", "", "updated_transport_project");
+$conn = new mysqli("localhost", "shreeinfotechsof", ",oSjcFMm,Rg;", "updated_transport_project");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
