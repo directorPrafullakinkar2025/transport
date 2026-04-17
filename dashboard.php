1f5de6e98dbdb5cc85c52146b54c05c147580f25
prafulla <directorprafullakinkar2025@gmail.com>
2026-04-17 12:15:10 +0530

    k

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@ header("Expires: 0");
 <meta charset="UTF-8">
 <title>Broker Slip Status</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/assets/css/layout.css">
+
 <style>
     .header-box {
     padding: 15px;
